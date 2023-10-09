37527aaf25198f4f82d03a180cff08aa63bbcb45
Cris Kiri <cris.kiri@gmail.com>
2023-10-09 22:08:00 +1300

    feat: add ui elements

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ export default function App() {
     <>
       <h1>Pompom</h1>
       <h2>Pomodoro timer</h2>
+      <div className="time">25:00</div>
+      <div className="flex-container">
+        <button className="start-stop">Start</button>
+        <button className="reset">Reset</button>
+        <button className="skip">Skip</button>
+      </div>
     </>
   )
 }
